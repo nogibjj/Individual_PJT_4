2a39fc13b6f81999639446c92e39dcfdf889c1f6
jiwonny29 <141780408+jiwonny29@users.noreply.github.com>
2023-12-18 15:21:04 +0000

    updates

--- app.py
+++ app.py
@@ -5,69 +5,59 @@ import os
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-client = OpenAI(api_key = OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 def request_rep(user_input):
-    prompt = user_input + ". Can you recommend me 3 foods specifying the restaurant chains?"
+    prompt = f"{user_input}. Can you recommend me 3 foods specifying the restaurant chains?"
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-    {"role": "user", 
-     "content": prompt}
-    ]
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
     )
     return completion.choices[0].message.content
 
-
 def parse_food(response):
-    '''
-    This function parses a text response from chatGPT and 
-    returns a list of food names and restaurants
-    input: a string of chatGPT response
-    output: a list of recommended foods
-    '''
     sentences = [x.split(" ") for x in response.split("\n")]
-    output = list()
-    for sent in sentences:        
-        split_ind = None        
+    output = []
+    
+    for sent in sentences:
+        split_ind = None
+
         if len(sent) <= 1:
-            pass
+            continue
         else:
             for i in range(len(sent)):
-                if sent[i] == ":":
+                if sent[i] in (":", "-") or sent[i][-1] == ":":
                     split_ind = i
-                elif sent[i][-1] == ":":
-                    split_ind = i
-                elif sent[i] == "-":
-                    split_ind = i
-            
-            sentStart = sent[0][:-1]
-            
-            try:
-                sentStart = int(sentStart)
-            except:
-                split_ind = None
-        if split_ind != None:
-            output.append(" ".join(sent[:split_ind]))            
-    return output
-    
 
+            sent_start = sent[0][:-1]
+
+            try:
+                sent_start = int(sent_start)
+            except ValueError:
+                split_ind = None
+
+        if split_ind is not None:
+            output.append(" ".join(sent[:split_ind]))
+
+    return output
 
 app = Flask(__name__)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    if request.method == "POST":        
-        feeling = request.form["feeling"]
-        if len(feeling) ==0 :
+    if request.method == "POST":
+        feeling = request.form.get("feeling", "")
+        
+        if not feeling:
             return render_template("home.html")
         else:
-            GPTresponse = request_rep(feeling)
-            answer = parse_food(GPTresponse)
-            return render_template("response.html", gpt_response = answer)
+            GPT_response = request_rep(feeling)
+            answer = parse_food(GPT_response)
+            return render_template("response.html", gpt_response=answer)
     else:
         return render_template("home.html")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9000)
